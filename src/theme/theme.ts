e1203749d7e80f61a0e2352ec3030cc22fd5d4aa
ykjellin <127968935+ykjellin@users.noreply.github.com>
2024-12-21 00:32:19 +0100

    made a theme for the styling

---
+++ src/theme/theme.ts
@@ -0,0 +1,20 @@
+import "@fontsource/montserrat-subrayada";
+import "@fontsource/montserrat";
+
+export const theme = {
+  colors: {
+    primary: "#2C3E50",
+    secondary: "#F1F1F1",
+    background: "#FFFFFF",
+    text: "#000000",
+  },
+  fonts: {
+    primary: "'Montserrat', sans-serif",
+    secondary: "'Montserrat Subrayada', 'Lato', sans-serif",
+  },
+  spacing: {
+    small: "0.5rem",
+    medium: "1rem",
+    large: "2rem",
+  },
+};
